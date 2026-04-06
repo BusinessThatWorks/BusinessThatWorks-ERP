0dabfb8d6dce371092c3b32ff5514439c00555b7
beetashoke <beetashoke.chakraborty@clapgrow.com>
2026-04-06 17:01:31 +0530

    doctype

---
+++ business_that_works_erp/business_that_works_erp/doctype/proposal/proposal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Clapgrow Developer and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Proposal", {
+// 	refresh(frm) {
+
+// 	},
+// });
